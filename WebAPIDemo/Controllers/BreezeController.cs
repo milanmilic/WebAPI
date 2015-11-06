0156cc40ca44e7505dd9539d2937d7e644a0cdc5
Milan Milic <milan_milic@outlook.com>
2015-11-06 13:42:24 +0100

    ovo je samo breeze kontroler - NE RADI
    ne znam jos u cemu je problem.... :(

---
+++ WebAPIDemo/Controllers/BreezeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Breeze.WebApi2;
+using System.Web.Http;
+using WebAPIDemo.Models;
+using Breeze.ContextProvider;
+using Newtonsoft.Json.Linq;
+
+namespace WebAPIDemo.Controllers
+{
+    [BreezeController]
+    public class BreezeController : ApiController
+    {
+        private readonly IRepository _repo;
+
+        public BreezeController(IRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public string Metadata()
+        {
+            return _repo.Metadata;
+        }
+        [HttpPost]
+        public SaveResult SaveChanges(JObject saveBundle)
+        {
+            return _repo.SaveChanges(saveBundle);
+        }
+        [HttpGet]
+        public IQueryable<Book> Books()
+        {
+            return _repo.Books();
+        }
+        [HttpGet]
+        public IQueryable<Order> Orders()
+        {
+            return _repo.Orders();
+        }
+    }
+}
